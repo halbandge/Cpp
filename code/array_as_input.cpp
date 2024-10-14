1f899e17424b804663e378c8168648d6b15f9162
thesohamhalbandge06 <logzii123@gmail.com>
2024-10-14 13:38:40 +0530

    commit

--- code/array_as_input.cpp
+++ code/array_as_input.cpp
@@ -1,14 +1,31 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
     int n;
-    cout << "Enter the number of elements:";
+    cout << "Enter the number of elements: ";
     cin >> n;
+
+    // Static array with size determined at runtime
     int arr[n];
 
-    for (int i = 0; i < n; i += 1)
+    // Input array elements
+    cout << "Enter the elements:" << endl;
+    for (int i = 0; i < n; i++)
         cin >> arr[i];
+
+    // Display the first 5 elements or all elements if n < 5
+    cout << "First 5 elements (or all if less than 5): ";
+    for (int i = 0; i < n && i < 5; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+
+    // Reversing and displaying the array
+    cout << "Reversed array: ";
+    for (int i = n - 1; i >= 0; i--)
+        cout << arr[i] << " ";
+    cout << endl;
 
     return 0;
 }
